1e1a713a3e87d754466487959bee87bb3df3b761
luoxuwei <xwluo@foxmail.com>
2022-08-23 23:19:52 +0800

    feat:开发搜索文档的方法
    一种搜所有图书的文档，另一种搜某一本图书里的文档

--- models/elasticsearch.go
+++ models/elasticsearch.go
@@ -122,3 +122,76 @@ func ElasticSearchBook(kw string, pageSize, page int) ([]int, int, error) {
 	}
 	return ids, count, err
 }
+
+func ElasticSearchDocument(kw string, pageSize, page int, bookId ...int) ([]int, int, error) {
+	var ids []int
+	count := 0
+
+	if page > 0 {
+		page = page - 1
+	} else {
+		page = 0
+	}
+	//搜所有图书的文档
+	queryJson := `
+		{
+		    "query" : {
+		        "match" : {
+		        	"release":"%v"
+		        }
+		    },
+		    "_source":["document_id"],
+			"size": %v,
+			"from": %v
+		}
+	`
+	queryJson = fmt.Sprintf(queryJson, kw, pageSize, page)
+
+	//搜某一本图书里的文档
+	if len(bookId) > 0 && bookId[0] > 0 {
+		queryJson = `
+			{
+				"query": {
+					"bool": {
+						"filter": [{
+							"term": {
+								"book_id":%v
+							}
+						}],
+						"must": {
+							"multi_match": {
+								"query": "%v",
+								"fields": ["release"]
+							}
+						}
+					}
+				},
+				"from": %v,
+				"size": %v,
+				"_source": ["document_id"]
+			}
+		`
+
+		queryJson = fmt.Sprintf(queryJson, kw, pageSize, page)
+	}
+
+	//elasticsearch api
+	host, _ := beego.AppConfig.String("elastic_host")
+	api := host + "mdocuments/datas/_search"
+
+	fmt.Println(api)
+	fmt.Println(queryJson)
+
+	sj, err := utils.HttpPostJson(api, queryJson)
+	if nil == err {
+		count = sj.GetPath("hits", "total").MustInt()
+		resultArray := sj.GetPath("hits", "hits").MustArray()
+		for _, v := range resultArray {
+			if each_map, ok := v.(map[string]interface{}); ok {
+				id, _ := strconv.Atoi(each_map["_id"].(string))
+				ids = append(ids, id)
+			}
+		}
+	}
+	return ids, count, err
+}
